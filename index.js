5ec0780102bcf8b91f54f838344e0a75ce8b2a07
Arjun <nagarjun2695@gmail.com>
2024-09-03 16:45:59 +0200

    added pony

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const emojis = { dog: "🐶", cat: "😺", pig:"🐷"};
+const emojis = { dog: "🐶", cat: "😺", pig:"🐷", pony: "🐴"};
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
