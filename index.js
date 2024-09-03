c0678d1da9320bda16de540cafe235e69692f2d2
Arjun <nagarjun2695@gmail.com>
2024-09-03 15:48:37 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const emojis = { dog: "🐶", cat: "😺" };
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.get('/emoji/:animal', (req, res) => {
+    res.send(emojis[req.params['animal']])
+})
+
+app.listen(port, () => {
+    console.log("listening at http://localhost:" + port);
+})
